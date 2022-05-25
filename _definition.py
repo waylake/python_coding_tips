f2f47cc0da25220805de8075f6aa8eae6d9ed0b3
waylake <hwangdoyeonyu@gmail.com>
2022-05-25 20:54:33 +0900

    add _definition.py

---
+++ _definition.py
@@ -0,0 +1,23 @@
+"""
+Tips for when create functions.
+"""
+
+import typing
+
+
+def exapmple_0() -> None:
+    """
+    Hinting the return value
+    Same expression
+    """
+
+    pass
+
+
+def exapmple_1() -> None:
+    """
+    Hinting the return value
+    Same expression
+    """
+
+    ...
